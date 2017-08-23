a80619781b6a9e122899e2e37f290047fe438a2a
wataru <837880110@qq.com>
2017-08-23 19:21:57 +0800

    添加 logback

--- home/src/main/java/com/keyou/fdcda/home/controller/IndexController.java
+++ home/src/main/java/com/keyou/fdcda/home/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.keyou.fdcda.home.controller;
 
 import com.keyou.fdcda.api.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +19,13 @@ import java.util.Map;
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    private final static Logger logger = LoggerFactory.getLogger(IndexController.class);
     @Autowired
     UserService userService;
 
     @RequestMapping("")
     public String index(Model model) {
+        logger.info("logback");
         model.addAttribute("info", userService.test());
         return "index";
     }
