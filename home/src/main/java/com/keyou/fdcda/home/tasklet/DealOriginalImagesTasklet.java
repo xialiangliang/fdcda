0f7cd7823a40c9589694b186cc3cb96d79aa5710
xialiangliang <xll@tzg.cn>
2017-11-20 14:39:40 +0800

    设备图片同步

--- home/src/main/java/com/keyou/fdcda/home/tasklet/DealOriginalImagesTasklet.java
+++ home/src/main/java/com/keyou/fdcda/home/tasklet/DealOriginalImagesTasklet.java
@@ -1,16 +1,22 @@
 package com.keyou.fdcda.home.tasklet;
 
+import java.util.Date;
+
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+
+import com.keyou.fdcda.api.utils.DateUtil;
 
 public class DealOriginalImagesTasklet  implements Tasklet{
 
 	@Override
 	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
 		
-		return null;
+		System.out.println("任务执行："+ DateUtil.getDate(DateUtil.DATETIME_FORMAT));
+		
+		return RepeatStatus.FINISHED;
 	}
 
 }
