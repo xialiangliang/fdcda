3e0121cf09d111896b474da941751521f2e54bc6
xialiangliang <xll@tzg.cn>
2017-11-26 14:17:35 +0800

    修改上传路径

--- api/src/main/java/com/keyou/fdcda/api/constants/ImageInfoConstants.java
+++ api/src/main/java/com/keyou/fdcda/api/constants/ImageInfoConstants.java
@@ -6,4 +6,11 @@ public interface ImageInfoConstants {
 	
 	/**图片状态：已处理*/
 	Integer IMAGE_STATUS_has_deal = 1;
+	
+	/**经营户上传采购商图片的根目录*/
+	String  UPLOAD_IMAGE_FILE_BASE_PATH = "/mnt/facepics/customer/";
+	
+	/**经营户上传升级黑名单证据根目录*/
+	String  UPLOAD_EVIDENCE_FILE_BASE_PATH = "/mnt/facepics/evidence/";
+	 
 }
