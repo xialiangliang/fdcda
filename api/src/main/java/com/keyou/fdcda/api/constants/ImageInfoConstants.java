383d772e6da2408429d07d12f51a95614dc332c0
xialiangliang <xll@tzg.cn>
2017-11-28 19:43:56 +0800

    增加首页

--- api/src/main/java/com/keyou/fdcda/api/constants/ImageInfoConstants.java
+++ api/src/main/java/com/keyou/fdcda/api/constants/ImageInfoConstants.java
@@ -12,5 +12,37 @@ public interface ImageInfoConstants {
 	
 	/**经营户上传升级黑名单证据根目录*/
 	String  UPLOAD_EVIDENCE_FILE_BASE_PATH = "/mnt/facepics/evidence/";
+	
+	/**图片根目录*/
+	String rootPath = "/mnt/facepics/";
+	
+	/**未处理图片根目录*/
+	String originalPath = "original";
+	
+	/**已处理图片根目录*/
+	String dealPath = "deal";
+	
+	/**0未识别出来的普通访客*/
+	int VISIT_TYPE_0 = 0;
+	
+	/**1已识别出来的普通访客*/
+	int VISIT_TYPE_1 = 1;
+	
+	/**2经营户的会员*/
+	int VISIT_TYPE_2 = 2;
+	
+	/**3本店设置的黑名单**/
+	int VISIT_TYPE_3 = 3;
+	
+	/**4系统黑名单*/
+	int VISIT_TYPE_4 = 4;
+	
+	/**5系统vip*/
+	int VISIT_TYPE_5 = 5;
+	
+	
+	
+	
+	
 	 
 }
--- home/src/main/java/com/keyou/fdcda/home/controller/VisitRecordInfoController.java
+++ home/src/main/java/com/keyou/fdcda/home/controller/VisitRecordInfoController.java
@@ -1,5 +1,12 @@
 package com.keyou.fdcda.home.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.keyou.fdcda.api.constants.Constants;
+import com.keyou.fdcda.api.constants.ImageInfoConstants;
+import com.keyou.fdcda.api.model.SysUser;
 import com.keyou.fdcda.api.model.VisitRecordInfo;
 import com.keyou.fdcda.api.model.base.PageResult;
 import com.keyou.fdcda.api.model.base.PaginationQuery;
@@ -19,18 +28,18 @@ import com.keyou.fdcda.home.controller.base.BaseController;
 @RequestMapping("/visitRecordInfo")
 public class VisitRecordInfoController extends BaseController {
 	private static Logger log = Logger.getLogger(VisitRecordInfoController.class);
-	
+
 	@Autowired
 	private VisitRecordInfoService visitRecordInfoService;
-	
-	@RequestMapping(value="/new")
-	public String add() throws Exception {		
+
+	@RequestMapping(value = "/new")
+	public String add() throws Exception {
 		return "/visitRecordInfo/new";
 	}
-	
-	@RequestMapping(value="/find")	
-	public String find(Long id, Model model){
-		try {	
+
+	@RequestMapping(value = "/find")
+	public String find(Long id, Model model) {
+		try {
 			VisitRecordInfo visitRecordInfo = visitRecordInfoService.findById(id);
 			model.addAttribute("param", visitRecordInfo);
 			model.addAttribute(Constants.SUCCESS, true);
@@ -42,9 +51,9 @@ public class VisitRecordInfoController extends BaseController {
 			return "/visitRecordInfo/update";
 		}
 	}
-	
-	@RequestMapping(value="/save")
-	public String save(@ModelAttribute("visitRecordInfo") VisitRecordInfo visitRecordInfo,Model model) {		
+
+	@RequestMapping(value = "/save")
+	public String save(@ModelAttribute("visitRecordInfo") VisitRecordInfo visitRecordInfo, Model model) {
 		try {
 			visitRecordInfoService.save(visitRecordInfo);
 			model.addAttribute(Constants.SUCCESS, true);
@@ -57,9 +66,10 @@ public class VisitRecordInfoController extends BaseController {
 			return "/visitRecordInfo/new";
 		}
 	}
-	
-	@RequestMapping(value="/update")
-	public String update(@ModelAttribute("visitRecordInfo") VisitRecordInfo visitRecordInfo,Model model) throws Exception {		
+
+	@RequestMapping(value = "/update")
+	public String update(@ModelAttribute("visitRecordInfo") VisitRecordInfo visitRecordInfo, Model model)
+			throws Exception {
 		try {
 			visitRecordInfoService.update(visitRecordInfo);
 			model.addAttribute(Constants.SUCCESS, true);
@@ -72,9 +82,9 @@ public class VisitRecordInfoController extends BaseController {
 			return "/visitRecordInfo/update";
 		}
 	}
-	
-	@RequestMapping(value="/delete")
-	public String delete(Long id,Model model) throws Exception {
+
+	@RequestMapping(value = "/delete")
+	public String delete(Long id, Model model) throws Exception {
 		try {
 			visitRecordInfoService.deleteById(id);
 			model.addAttribute(Constants.SUCCESS, true);
@@ -86,15 +96,73 @@ public class VisitRecordInfoController extends BaseController {
 			return "redirect:/visitRecordInfo";
 		}
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String list(PaginationQuery query,Model model) throws Exception{		
+	public String list(PaginationQuery query, Model model) throws Exception {
 		PageResult<VisitRecordInfo> pageList = visitRecordInfoService.findPage(query);
 		model.addAttribute("result", pageList);
 		model.addAttribute("query", query.getQueryData());
 		return "/visitRecordInfo/list";
 	}
-	
+
+	@RequestMapping(value = "/visitIndex",method = RequestMethod.GET)
+	public String index(HttpServletRequest request, Model model) throws Exception {
+		SysUser sysUser = getUser(request);
+		if (sysUser == null) {
+			return "redirect:/login";
+		}
+		Map<String, Object> map = new HashMap<>();
+		map.put("createToday", "1");
+		map.put("userRowId", sysUser.getId());
+		List<VisitRecordInfo> list = visitRecordInfoService.findAllPage(map);
+		// 总访问量
+		Integer totalCount = 0;
+		// 可疑人员数
+		List<VisitRecordInfo> blackList = new ArrayList<>();
+
+		// 会员数
+		List<VisitRecordInfo> vipList = new ArrayList<>();
+		// 普通访客
+		List<VisitRecordInfo> normalList = new ArrayList<>();
+
+		if (list != null && !list.isEmpty()) {
+			totalCount = list.size();
+			for (VisitRecordInfo info : list) {
+				Integer type = info.getVisitType();
+				if (type != null) {
+					switch (type) {
+					case ImageInfoConstants.VISIT_TYPE_0:
+						normalList.add(info);
+						break;
+					case ImageInfoConstants.VISIT_TYPE_1:
+						normalList.add(info);
+						break;
+					case ImageInfoConstants.VISIT_TYPE_2:
+						vipList.add(info);
+						break;
+					case ImageInfoConstants.VISIT_TYPE_3:
+						blackList.add(info);
+						break;
+					case ImageInfoConstants.VISIT_TYPE_4:
+						blackList.add(info);
+						break;
+					case ImageInfoConstants.VISIT_TYPE_5:
+						vipList.add(info);
+						break;
+						
+					default:
+						break;
+					}
+				}
+			}
+		}
+		model.addAttribute("totalCount", totalCount);
+		model.addAttribute("blackCount", blackList.size());
+		model.addAttribute("vipCount", vipList.size());
+		model.addAttribute("normalList", normalList);
+		model.addAttribute("vipList", vipList);
+		model.addAttribute("totalList", list);
+		
+		return "/page/index";
+	}
 }
-
-
