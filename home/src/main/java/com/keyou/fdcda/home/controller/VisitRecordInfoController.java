bca1837142d500dde2a5ebf6b0095c95a9fc8e4a
xialiangliang <xll@tzg.cn>
2017-11-30 17:19:14 +0800

    首页路径修改

--- home/src/main/java/com/keyou/fdcda/home/controller/VisitRecordInfoController.java
+++ home/src/main/java/com/keyou/fdcda/home/controller/VisitRecordInfoController.java
@@ -1,11 +1,5 @@
 package com.keyou.fdcda.home.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,13 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.keyou.fdcda.api.constants.Constants;
-import com.keyou.fdcda.api.constants.ImageInfoConstants;
-import com.keyou.fdcda.api.model.SysUser;
 import com.keyou.fdcda.api.model.VisitRecordInfo;
 import com.keyou.fdcda.api.model.base.PageResult;
 import com.keyou.fdcda.api.model.base.PaginationQuery;
 import com.keyou.fdcda.api.service.VisitRecordInfoService;
-import com.keyou.fdcda.api.utils.StringUtil;
 import com.keyou.fdcda.home.controller.base.BaseController;
 
 @Controller
@@ -105,81 +96,5 @@ public class VisitRecordInfoController extends BaseController {
 		return "/visitRecordInfo/list";
 	}
 	
-	private void dealUrl(List<VisitRecordInfo> list){
-		if (list != null && !list.isEmpty()) {
-			for (VisitRecordInfo info : list) {
-				Integer type = info.getVisitType();
-				if (type != null&&StringUtil.isNotBlank(info.getImageUrl())) {
-					String url = info.getImageUrl();
-					info.setImageUrl(ImageInfoConstants.STATIC_IMAGE_SERVER_URL+url.substring(url.indexOf("deal")));
-					 
-				}
-			}
-		}
-	}
-
-	@RequestMapping(value = "/visitIndex",method = RequestMethod.GET)
-	public String index(HttpServletRequest request, Model model) throws Exception {
-		SysUser sysUser = getUser(request);
-		if (sysUser == null) {
-			return "redirect:/login";
-		}
-		Map<String, Object> map = new HashMap<>();
-		map.put("createToday", "1");
-		map.put("userRowId", sysUser.getId());
-		// 总访问量
-		Long totalCount = visitRecordInfoService.findPageCount(map);
-		
-		map.put("visitType", ImageInfoConstants.VISIT_TYPE_2);
-		Long vipCount = visitRecordInfoService.findPageCount(map);
-		
-		map.put("visitType", ImageInfoConstants.VISIT_TYPE_1);
-		Long normalCount = visitRecordInfoService.findPageCount(map);
-		
-		// 未识别普通访客
-	    map.put("visitType", ImageInfoConstants.VISIT_TYPE_0);
-	    Long normalNotCount = visitRecordInfoService.findPageCount(map);
-		
-	    // 可疑人员数
-	    map.remove("visitType");
-		map.put("visitTypeblack", "1");
-		Long blackCount = visitRecordInfoService.findPageCount(map);
-		 map.remove("visitTypeblack");
-	    
-		map.put("endRecord", 10);
-		map.put("startRecord", 0);
-		// 会员数
-		map.put("visitType", ImageInfoConstants.VISIT_TYPE_2);
-		List<VisitRecordInfo> vipList = visitRecordInfoService.findAllPage(map);
-		// 已识别普通访客
-		map.put("visitType", ImageInfoConstants.VISIT_TYPE_1);
-	    List<VisitRecordInfo> normalList = visitRecordInfoService.findAllPage(map);
-	    
-	    // 未识别普通访客
-	    map.put("visitType", ImageInfoConstants.VISIT_TYPE_0);
-	    List<VisitRecordInfo> normalNotList = visitRecordInfoService.findAllPage(map);
-		
-		// 可疑人员数
-	    map.remove("visitType");
-		map.put("visitTypeblack", "1");
-		List<VisitRecordInfo> blackList = visitRecordInfoService.findAllPage(map);
-		
-
-		dealUrl(blackList);
-		dealUrl(vipList);
-		dealUrl(normalList);
-		dealUrl(normalNotList);
-		
-		model.addAttribute("totalCount", totalCount);
-		model.addAttribute("blackCount", blackCount);
-		model.addAttribute("blackList", blackList);
-		model.addAttribute("vipCount", vipCount);
-		model.addAttribute("normalList", normalList);
-		model.addAttribute("normalCountt", normalCount);
-		model.addAttribute("vipList", vipList);
-		model.addAttribute("normalNotList", normalNotList);
-		model.addAttribute("normalNotCount", normalNotCount);
-		
-		return "/page/index";
-	}
+	
 }
