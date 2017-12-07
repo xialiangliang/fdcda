5126a472d5431c89ab455d21a2412930cb892dba
zzq <837880110@qq.com>
2017-12-07 22:50:25 +0800

    区域管理

--- home/src/main/webapp/WEB-INF/static/js/page/sysAreaInfo/list.js
+++ home/src/main/webapp/WEB-INF/static/js/page/sysAreaInfo/list.js
@@ -1,6 +1,41 @@
 $(function(){
     var dialog = $("#newUpdateDialogFrame");
+    layui.use(['laydate', 'table'], function(){
+        var laydate = layui.laydate, table = layui.table;
 
+        //执行一个laydate实例
+        laydate.render({
+            elem: '#createTimeBeginStr' //指定元素
+            ,value: new Date()
+        });
+        laydate.render({
+            elem: '#createTimeEndStr' //指定元素
+            ,value: new Date()
+        });
+
+        table.render({
+            elem: '#test'
+            ,url:'/sysAreaInfo/listJson'
+            ,height:500
+            ,cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+            , page:true
+            ,cols: [[
+                {field:'code', title: '区域code', width:160}
+                ,{field:'parentcode', title: '父区域code', width:160}
+                ,{field:'areaname', title: '名称', width:80}
+                ,{fixed:'right',  align:'center', toolbar: '#barDemo', title:'操作', width:'15%'}
+            ]]
+        });
+
+        //监听工具条
+        table.on('tool(demo)', function(obj){
+            var data = obj.data;
+            if(obj.event === 'detail'){
+            }
+        });
+
+
+    });
     $(".j_update-btn").click(function () {
         $.get('/sysAreaInfo/find', {'id': $(this).attr("data-id")}, function (data, textStatus, object) {
             layer.open({
